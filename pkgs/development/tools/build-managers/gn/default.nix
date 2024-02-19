641947cb8f74856fbdbab7bcca0deabe88152c6d
Matt Leon <ml@mattleon.com>
2024-02-19 18:45:21 -0500

    Update gn

--- pkgs/development/tools/build-managers/gn/default.nix
+++ pkgs/development/tools/build-managers/gn/default.nix
@@ -3,8 +3,8 @@
 callPackage ./generic.nix args {
   # Note: Please use the recommended version for Chromium, e.g.:
   # https://git.archlinux.org/svntogit/packages.git/tree/trunk/chromium-gn-version.sh?h=packages/gn
-  rev = "fd3d768bcfd44a8d9639fe278581bd9851d0ce3a";
-  revNum = "1718"; # git describe HEAD --match initial-commit | cut -d- -f3
-  version = "2020-03-09";
-  sha256 = "1asc14y8by7qcn10vbk467hvx93s30pif8r0brissl0sihsaqazr";
+  rev = "8b973aa51d02aa1ab327100007d4070c24b862b0";
+  revNum = "2145"; # git describe HEAD --match initial-commit | cut -d- -f3
+  version = "2024-02-15";
+  sha256 = "sha256-SD4IJFPsJoMqVCXfzlcdzBwszBxm1mgsw82GimUP9yo=";
 }
